14a7d3b08ac6ba23e1cbaaa8dbde670e6f791ca1
Isabella Obando <139140580+Isabella1911@users.noreply.github.com>
2024-01-29 09:37:40 -0600

    La estructura de las clases gnome y merge se ha subido

--- TestSorts.java
+++ TestSorts.java
@@ -1,3 +1,10 @@
+import static org.junit.Assert.assertArrayEquals;
+
 public class TestSorts{
-    
+    public void positiveTest() {
+        int[] actual = { 5, 1, 6, 2, 3, 4 };
+        int[] expected = { 1, 2, 3, 4, 5, 6 };
+        MergeSort.mergeSort(actual, actual.length);
+        assertArrayEquals(expected, actual);
+    }
 }

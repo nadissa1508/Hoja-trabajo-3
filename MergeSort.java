8f553d077fda4bb9be3e78ba6462ad62c1f7a392
Isabella Obando <139140580+Isabella1911@users.noreply.github.com>
2024-01-30 22:26:06 -0600

    se cambiaron las clases para usar datos genericos

--- MergeSort.java
+++ MergeSort.java
@@ -8,42 +8,43 @@
  * @date creación 23/01/2024 última modificación 30/01/2024
  */
 
-public class MergeSort {
-    public static void mergeSort(int[] a, int n) {
-        if (n < 2) {
-            return;
-        }
-        int mid = n / 2;
-        int[] l = new int[mid];
-        int[] r = new int[n - mid];
-
-        for (int i = 0; i < mid; i++) {
-            l[i] = a[i];
-        }
-        for (int i = mid; i < n; i++) {
-            r[i - mid] = a[i];
-        }
-        mergeSort(l, mid);
-        mergeSort(r, n - mid);
-
-        merge(a, l, r, mid, n - mid);
-    }
-
-    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
-
-        int i = 0, j = 0, k = 0;
-        while (i < left && j < right) {
-            if (l[i] <= r[j]) {
-                a[k++] = l[i++];
-            } else {
-                a[k++] = r[j++];
-            }
-        }
-        while (i < left) {
-            a[k++] = l[i++];
-        }
-        while (j < right) {
-            a[k++] = r[j++];
-        }
-    }
-}
+ import java.util.Arrays;
+ public class MergeSort<T extends Comparable<T>> implements ISort<T> {
+ 
+     @Override
+     public void sort(T[] array) {
+         mergeSort(array, array.length);
+     }
+ 
+     private void mergeSort(T[] a, int n) {
+         if (n < 2) {
+             return;
+         }
+         int mid = n / 2;
+         T[] l = Arrays.copyOfRange(a, 0, mid);
+         T[] r = Arrays.copyOfRange(a, mid, n);
+ 
+         mergeSort(l, mid);
+         mergeSort(r, n - mid);
+ 
+         merge(a, l, r, mid, n - mid);
+     }
+ 
+     private void merge(T[] a, T[] l, T[] r, int left, int right) {
+         int i = 0, j = 0, k = 0;
+         while (i < left && j < right) {
+             if (l[i].compareTo(r[j]) <= 0) {
+                 a[k++] = l[i++];
+             } else {
+                 a[k++] = r[j++];
+             }
+         }
+         while (i < left) {
+             a[k++] = l[i++];
+         }
+         while (j < right) {
+             a[k++] = r[j++];
+         }
+     }
+ }
+ 
--- RadixSort.java
+++ RadixSort.java
@@ -8,71 +8,53 @@
  * @date creación 23/01/2024 última modificación 30/01/2024
  */
 
-import java.io.*;
-import java.util.*;
+ import java.util.Arrays;
 
-public class RadixSort {
-    static int getMax(int arr[], int n) {
-        int mx = arr[0];
-        for (int i = 1; i < n; i++)
-            if (arr[i] > mx)
-                mx = arr[i];
-        return mx;
-    }
-
-    static void countSort(int arr[], int n, int exp) {
-        int output[] = new int[n]; // output array
-        int i;
-        int count[] = new int[10];
-        Arrays.fill(count, 0);
-
-        // Store count of occurrences in count[]
-        for (i = 0; i < n; i++)
-            count[(arr[i] / exp) % 10]++;
-
-        // Change count[i] so that count[i] now contains
-        // actual position of this digit in output[]
-        for (i = 1; i < 10; i++)
-            count[i] += count[i - 1];
-
-        // Build the output array
-        for (i = n - 1; i >= 0; i--) {
-            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-            count[(arr[i] / exp) % 10]--;
-        }
-
-        // Copy the output array to arr[], so that arr[] now
-        // contains sorted numbers according to current
-        // digit
-        for (i = 0; i < n; i++)
-            arr[i] = output[i];
-    }
-
-    public static void radixSort(int arr[], int n) {
-        // Find the maximum number to know number of digits
-        int m = getMax(arr, n);
-
-        // Do counting sort for every digit. Note that
-        // instead of passing digit number, exp is passed.
-        // exp is 10^i where i is current digit number
-        for (int exp = 1; m / exp > 0; exp *= 10)
-            countSort(arr, n, exp);
-    }
-
-    // A utility function to print an array
-    static void print(int arr[], int n) {
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
-    }
-
-    // // Main driver method
-    // public static void main(String[] args)
-    // {
-    // int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
-    // int n = arr.length;
-
-    // // Function Call
-    // radixsort(arr, n);
-    // print(arr, n);
-    // }
-}
+ public class RadixSort<T extends Comparable<T>> implements ISort<T> {
+     @Override
+     public void sort(T[] array) {
+         // Verifica si el array está vacío o tiene un solo elemento
+         if (array == null || array.length <= 1) {
+             return;
+         }
+ 
+         // Encuentra el elemento máximo para determinar el número de dígitos
+         T max = array[0];
+         for (T element : array) {
+             if (element.compareTo(max) > 0) {
+                 max = element;
+             }
+         }
+ 
+         // Realiza el radix sort
+         for (int exp = 1; ((Integer) max) / exp > 0; exp *= 10) {
+             countSort(array, exp);
+         }
+     }
+ 
+     private void countSort(T[] array, int exp) {
+         int n = array.length;
+         T[] output = Arrays.copyOf(array, n);
+ 
+         int[] count = new int[10];
+         Arrays.fill(count, 0);
+ 
+         for (int i = 0; i < n; i++) {
+             count[((Integer) array[i]) / exp % 10]++;
+         }
+ 
+         for (int i = 1; i < 10; i++) {
+             count[i] += count[i - 1];
+         }
+ 
+         for (int i = n - 1; i >= 0; i--) {
+             output[count[((Integer) array[i]) / exp % 10] - 1] = array[i];
+             count[((Integer) array[i]) / exp % 10]--;
+         }
+ 
+         System.arraycopy(output, 0, array, 0, n);
+     }
+ 
+    
+ }
+ 
